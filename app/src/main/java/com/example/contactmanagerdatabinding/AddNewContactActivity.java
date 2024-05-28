205f2089e49e7b1156226595a850cde54de352a2
Kolchin13D <kolchin13d@gmail.com>
2024-05-28 16:45:00 +0300

    add new classes

--- app/src/main/java/com/example/contactmanagerdatabinding/AddNewContactActivity.java
+++ app/src/main/java/com/example/contactmanagerdatabinding/AddNewContactActivity.java
@@ -1,4 +1,33 @@
 package com.example.contactmanagerdatabinding;
 
-public class AddNewContactActivity {
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddNewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_new_contact);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public class AddNewContactActivityHandler {
+        Context context;
+
+        public AddNewContactActivityHandler(Context context) {
+            this.context = context;
+        }
+    }
 }
